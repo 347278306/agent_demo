c99e99d63944fb1e11e85b16fd5a8b6d13b2d8c7
chenhao <chenhao@dlz-tech.com>
2026-03-03 10:35:46 +0800

    feat:commit

--- rag/rag_service.py
+++ rag/rag_service.py
@@ -1,6 +1,7 @@
 from langchain_core.documents import Document
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser, PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from pydantic import BaseModel
 
 from model.factory import chat_model
 from rag.vector_store import VectorStoreService
@@ -44,7 +45,34 @@ class RagSummarizeService(object):
             }
         )
 
+class Answer(BaseModel):
+    answer: str
+    source: list[str]
+
+class RagJsonService:
+    def __init__(self):
+        self.vector_store = VectorStoreService()
+        self.retriever = self.vector_store.get_retriever()
+        self.parser = PydanticOutputParser(pydantic_object=Answer)
+
+        # 在提示此种嵌入格式说明
+        self.prompt_template = PromptTemplate.from_template(
+            """基于一下参考资料回答问题。
+            参考资料：{context}
+            问题：{input}
+            {format_instructions}""",
+            partial_variables={"format_instructions": self.parser.get_format_instructions()}
+        )
+        self.chain = self.prompt_template | chat_model | self.parser
+
+    def rag_summarize(self, query: str) -> Answer:
+        docs = self.retriever.invoke(query)
+        context = "\n\n".join([doc.page_content for doc in docs])
+        return self.chain.invoke({"input": query, "context": context})
+
 if __name__ == '__main__':
-    rag = RagSummarizeService()
+    # rag = RagSummarizeService()
+
+    rag = RagJsonService()
     res = rag.rag_summarize("扫地机器人的电池一般能用多久？")
     print(res)
